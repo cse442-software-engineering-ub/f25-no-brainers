4b7e837b6c7911bd65c9de96e1a61e7638bfb65c
chris-sooseok <sooseokkim99@gmail.com>
2025-09-29 05:52:09 -0400

    Creating component of purchase history page #139 uploaded

--- dorm-mart/src/App.js
+++ dorm-mart/src/App.js
@@ -1,29 +1,29 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import RootLayout from './pages/RootLayout';
-import ItemDetailPage from './pages/PurchaseHistory/ItemDetailPage';
-import PurchaseHistoryPage from './pages/PurchaseHistory/PurchaseHistoryPage';
-import PurchaseHistoryLayout from './pages/PurchaseHistory/PurchaseHistoryLayout';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import RootLayout from "./pages/RootLayout";
+import ItemDetailPage from "./pages/PurchaseHistory/ItemDetailPage";
+import PurchaseHistoryPage from "./pages/PurchaseHistory/PurchaseHistoryPage";
+import PurchaseHistoryLayout from "./pages/PurchaseHistory/PurchaseHistoryLayout";
 
-export const router = createBrowserRouter([
-  {
-    path: "/",                
-    element: <RootLayout />,  
-    children: [
-      {
-        path: "purchase-history",
-        element: <PurchaseHistoryLayout />,
-        children: [
-          { index: true, element: <PurchaseHistoryPage /> },       
-          { path: "item-detail/:id", element: <ItemDetailPage /> },       
-        ],
-      },
-    ],
-  },
-]);
+export const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <RootLayout />,
+      children: [
+        {
+          path: "purchase-history",
+          element: <PurchaseHistoryLayout />,
+          children: [
+            { index: true, element: <PurchaseHistoryPage /> },
+            { path: "item-detail/:id", element: <ItemDetailPage /> },
+          ],
+        },
+      ],
+    },
+  ],
+  { basename: "/CSE442/2025-Fall/cse-442j" }
+);
 
-function App() {
-  return <RouterProvider router={router} />
+export default function App() {
+  return <RouterProvider router={router} />;
 }
-
-
-export default App;
