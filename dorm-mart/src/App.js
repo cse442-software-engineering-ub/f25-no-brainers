90cbe853e493aa5169b8872180cbf26d4cdc70a4
anishcse312 <anishbancse312@gmail.com>
2025-10-02 07:50:03 -0400

    Fixed routing in App.js
    Included routing and path for Create Account at "/create-account"

--- dorm-mart/src/App.js
+++ dorm-mart/src/App.js
@@ -1,28 +1,37 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import RootLayout from './pages/RootLayout';
+import LoginPage from './pages/LoginPage';
+import HomePage from './pages/HomePage';
 import ItemDetailPage from './pages/PurchaseHistory/ItemDetailPage';
 import PurchaseHistoryPage from './pages/PurchaseHistory/PurchaseHistoryPage';
 import PurchaseHistoryLayout from './pages/PurchaseHistory/PurchaseHistoryLayout';
-import AccountCreatePage from './pages/AccountCreation/index.jsx'
+import CreateAccount from './pages/AccountCreation/index.jsx'
 
 export const router = createBrowserRouter([
+  // Login is the default route
   {
-    path: "/",                
-    element: <RootLayout />,  
+    path: "/",
+    element: <LoginPage />,
+  },
+  {
+    path:"/create-account",
+    element: <CreateAccount />,
+  },
+  // Main application lives under /app
+  {
+    path: "/app",
+    element: <RootLayout />,
     children: [
+      { index: true, element: <HomePage /> },
       {
         path: "purchase-history",
         element: <PurchaseHistoryLayout />,
         children: [
-          { index: true, element: <PurchaseHistoryPage /> },       
-          { path: "item-detail/:id", element: <ItemDetailPage /> },       
+          { index: true, element: <PurchaseHistoryPage /> },
+          { path: "item-detail/:id", element: <ItemDetailPage /> },
         ],
       },
     ],
-  },
-  {
-    path:"/create-account",
-    element: <AccountCreatePage />,
   },
 ]);
 
