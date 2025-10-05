033d1d49242ce5953c84a74416c9c53e8305a6bb
anishcse312 <anishbancse312@gmail.com>
2025-10-05 01:22:07 -0400

    Added code to create SQL Table
    Modified the db_connect.php file to create a table called "users" if it does not already exist. db_connect.php now also returns the $conn mysqli connection object variable when called.

--- db_connect.php
+++ db_connect.php
@@ -8,4 +8,8 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 echo "Connected successfully";
+
+$conn->query("CREATE TABLE IF NOT EXISTS Users(user_id INT PRIMARY KEY, first_name TEXT, last_name TEXT, email TEXT, grad_date VARCHAR(10), password_hash VARCHAR(255) NOT NULL, auth_has VARCHAR(255))");
+
+return $conn;
 ?>
