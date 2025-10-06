ffc5cdfef0127885be251fa2952a2076ee233767
anishcse312 <anishbancse312@gmail.com>
2025-10-05 20:20:57 -0400

    Switched to hash router
    Modified the App.js file to use the hash router instead of the browser router.

--- dorm-mart/src/App.js
+++ dorm-mart/src/App.js
@@ -1,20 +1,40 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import RootLayout from './pages/RootLayout';
-import ItemDetailPage from './pages/PurchaseHistory/ItemDetailPage';
-import PurchaseHistoryPage from './pages/PurchaseHistory/PurchaseHistoryPage';
-import PurchaseHistoryLayout from './pages/PurchaseHistory/PurchaseHistoryLayout';
+import { createHashRouter, RouterProvider } from "react-router-dom";
+import RootLayout from "./pages/RootLayout";
+import ItemDetailPage from "./pages/PurchaseHistory/ItemDetailPage";
+import PurchaseHistoryPage from "./pages/PurchaseHistory/PurchaseHistoryPage";
+import PurchaseHistoryLayout from "./pages/PurchaseHistory/PurchaseHistoryLayout";
 import CreateAccount from './pages/AccountCreation/index.jsx'
 
-export const router = createBrowserRouter([
+
+/* hashRouter adds # in front of each url path
+ Request: https://example.com/#/app/purchase-history.
+ Server only sees https://example.com/ and serves index.html.
+ The part after # (/app/purchase-history) is handled by JS routing in client-side */
+
+export const router = createHashRouter([
+
   {
-    path:"/create-account",
+    path: "/create-account",
     element: <CreateAccount />,
+  },
+  // Main application lives under /app
+  {
+    path: "/app",
+    element: <RootLayout />,
+    children: [
+      {
+        path: "purchase-history",
+        element: <PurchaseHistoryLayout />,
+        children: [
+          { index: true, element: <PurchaseHistoryPage /> },
+          { path: "item-detail/:id", element: <ItemDetailPage /> },
+        ],
+      },
+    ],
   },
 ]);
 
 function App() {
-  return <RouterProvider router={router} />
+  return <RouterProvider router={router} />;
 }
-
-
 export default App;
