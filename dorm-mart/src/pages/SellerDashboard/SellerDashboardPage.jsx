8ea2226ff323278b4c8350ae8c8a74893bd47487
Sameer Jain <sameerjain501@gmail.com>
2025-10-14 00:17:32 -0400

    added seller dashboard page + nav

---
+++ dorm-mart/src/pages/SellerDashboard/SellerDashboardPage.jsx
@@ -0,0 +1,9 @@
+function SellerDashboardPage() {
+    return (
+        <div className="min-h-screen bg-gray-50">
+            {/* Blank page - only navbar visible */}
+        </div>
+    );
+}
+
+export default SellerDashboardPage;
