1c7002c1decd85a00adf26b55f94f3d3e91995f2
chris-sooseok <sooseokkim99@gmail.com>
2025-10-14 19:51:56 -0400

    editing files to align tests for purchase history

--- dorm-mart/api/api-test-files/chris/no-purchased-item.php
+++ dorm-mart/api/api-test-files/chris/no-purchased-item.php
@@ -4,19 +4,7 @@ require __DIR__ . '/../../db_connect.php';
 
 $conn = db();
 
-$sql_clean_data = "TRUNCATE TABLE transacted_items";
-$conn->query($sql_clean_data);
-
-$sql = "SELECT *
-        FROM transacted_items
-        ORDER BY transacted_at DESC";
-
-$res = $conn->query($sql);
-
 $rows = [];
-while ($row = $res->fetch_assoc()) {
-    $rows[] = $row;
-}
 
 echo json_encode(['success' => true, 'data' => $rows]);
 
